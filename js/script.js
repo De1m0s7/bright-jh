bfc3b2089eeb12ed395ab3f68bbb9e95daa1339d
Aleksandr <89394956+De1m0s7@users.noreply.github.com>
2022-07-06 14:08:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,95 @@
+//---------------Animation on scroll-----------------------//
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('active');
+            } else {
+                if(!animItem.classList.contains('anim-no-hide')) {
+                    animItem.classList.remove('active');
+                }
+            }
+        }
+    }
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
+//---------------/Animation on scroll-----------------------//
+
+//Анимация счетчика
+window.addEventListener("load", windowLoad);
+
+function windowLoad() {
+    //Функция инициализации
+    function digitsCountersInit(digitsCountersItems) {
+        let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
+        if (digitsCounters) {
+            digitsCounters.forEach(digitsCounter => {
+                digitsCountersAnimate(digitsCounter);
+            });
+        }
+    }
+    //Функция анимации
+    function digitsCountersAnimate(digitsCounter) {
+        let startTimestamp = null;
+        const duration = parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounter) : 1000;
+        const startValue = parseInt(digitsCounter.innerHTML);
+        const startPosition = 0;
+        const step = (timestamp) => {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+            digitsCounter.innerHTML = Math.floor(progress * (startPosition + startValue));
+            if (progress < 1) {
+                window.requestAnimationFrame(step);
+            }
+        };
+        window.requestAnimationFrame(step);
+    }
+    //Запуск при загрузке страницы
+    //digitsCountersInit();
+
+    //Пуск при скролле (появление блока со счетчиком)
+    let options = {
+        threshold: 0.3
+    }
+    let observer = new IntersectionObserver((enteries, observer) => {
+        enteries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const targetElement = entry.target;
+                const digitsCountersItems = targetElement.querySelectorAll("[data-digits-counter]");
+                if (digitsCountersItems.length) {
+                    digitsCountersInit(digitsCountersItems);
+                }
+                //Выключить отслеживание после сработки
+                observer.unobserve(targetElement);
+            }
+        });
+    }, options);
+
+    let sections = document.querySelectorAll('.employment');
+    if (sections.length) {
+        sections.forEach(section => {
+            observer.observe(section);
+        });
+    }
+}
